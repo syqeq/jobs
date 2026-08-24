5ff8649372b3b2877b60383172d6a034bed6b64e
syqeq <basbastal@gmail.com>
2026-08-24 22:31:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -156,8 +156,7 @@ app.get("/api/uploads/:filename", (req, res) => {
     res.status(404).send("الملف غير موجود.");
   }
 });
-
-if (process.env.NODE_ENV !== "production" && !process.env.VERCEL) {
+if (!process.env.VERCEL) {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
